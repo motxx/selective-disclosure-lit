5929480a5370e29928b701b70ae6383a5702fbff
motxx <motohiko.ave@gmail.com>
2023-07-09 16:23:33 +0900

    style: prettier solidity

--- blockchain/contracts/RevocationTree.sol
+++ blockchain/contracts/RevocationTree.sol
@@ -5,31 +5,27 @@ pragma solidity ^0.8.4;
 import "fuel-merkle-sol/contracts/tree/sparse/SparseMerkleTree.sol";
 
 contract RevocationTree {
-    bytes32 public root;
+  bytes32 public root;
 
-    function verifyCompact(
-        SparseCompactMerkleProof memory _proof,
-        bytes32 _key,
-        bytes memory _value,
-        bytes32 _root
-    ) public pure returns (bool) {
-        return SparseMerkleTree.verifyCompact(_proof, _key, _value, _root);
-    }
+  function verifyCompact(
+    SparseCompactMerkleProof memory _proof,
+    bytes32 _key,
+    bytes memory _value,
+    bytes32 _root
+  ) public pure returns (bool) {
+    return SparseMerkleTree.verifyCompact(_proof, _key, _value, _root);
+  }
 
-    function addBranchesAndUpdate(
-        MerkleBranch[] memory _branches,
-        bytes32 _root,
-        bytes32 _key,
-        bytes memory _value
-    ) public {
-        root = SparseMerkleTree.addBranchesAndUpdate(_branches, _root, _key, _value);
-    }
+  function addBranchesAndUpdate(
+    MerkleBranch[] memory _branches,
+    bytes32 _root,
+    bytes32 _key,
+    bytes memory _value
+  ) public {
+    root = SparseMerkleTree.addBranchesAndUpdate(_branches, _root, _key, _value);
+  }
 
-    function addBranchesAndDelete(
-        MerkleBranch[] memory _branches,
-        bytes32 _root,
-        bytes32 _key
-    ) public {
-        root = SparseMerkleTree.addBranchesAndDelete(_branches, _root, _key);
-    }
+  function addBranchesAndDelete(MerkleBranch[] memory _branches, bytes32 _root, bytes32 _key) public {
+    root = SparseMerkleTree.addBranchesAndDelete(_branches, _root, _key);
+  }
 }
